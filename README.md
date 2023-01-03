87a7ece95a656fc9fab338fc39ebd076fa9f4b43
Girish-Bharadwaj <girishkulkarni351@gmail.com>
2023-01-03 23:43:35 +0530

    fix

---
+++ README.md
@@ -0,0 +1,5 @@
+### Execute the following commands everytime u pull/push to the repo
+
+### While pushing: ./manage.py dumpdata > db.json
+
+### After pulling: ./manage.py loaddata db.json
--- placement/models.py
+++ placement/models.py
@@ -8,4 +8,5 @@ class Student(models.Model):
     password = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
+
     
