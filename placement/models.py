232136b924c6b36df8c71ea3b549c2e26e1a2def
Girish-Bharadwaj <girishkulkarni351@gmail.com>
2023-01-03 22:17:23 +0530

    connected postgres

--- placement/models.py
+++ placement/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    
