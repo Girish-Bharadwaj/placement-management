7d61917641bbc94222344cb523cdf49c65e2a4a2
Girish-Bharadwaj <girishkulkarni351@gmail.com>
2023-01-19 23:02:27 +0530

    implemented edit profile

--- placement/models.py
+++ placement/models.py
@@ -1,12 +1,38 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
 
-class Student(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    phone = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    ctc=models.IntegerField()
+    stipend=models.IntegerField()
+    branches=models.CharField(max_length=50)
+    btech_criteria=models.CharField(max_length=50)
+    sem_criteria=models.CharField(max_length=50)
+    location=models.CharField(max_length=50)
+    date=models.DateField(default=datetime.now)
+    date_of_drive=models.DateField(blank=True, null=True)
+    def __str__(self):
+        return self.name
 
-    
+class job_profile(models.Model):
+    company=models.ForeignKey(Company, on_delete=models.CASCADE)
+    profile=models.CharField(max_length=50)
+    def __str__(self):
+        return self.profile
+
+class registered_companies(models.Model):
+    company=models.ForeignKey(Company, on_delete=models.CASCADE)
+    student=models.ForeignKey(User, on_delete=models.CASCADE)
+    profile=models.ForeignKey(job_profile, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.company.name} - {self.student.username} - {self.profile.profile}'
+
+class placed_students(models.Model):
+    student=models.ForeignKey(User, on_delete=models.CASCADE)
+    company=models.ForeignKey(Company, on_delete=models.CASCADE)
+    profile=models.ForeignKey(job_profile, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.company.name
